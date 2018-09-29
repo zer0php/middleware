cbf1b7fe49f68684093587873f358c7314c85dda
Mohos Tamás <tomi@mohos.name>
2018-09-30 01:11:09 +0200

    feat(Middleware): add MissingResponseException class

---
+++ src/Middleware/Exception/MissingResponseException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Zero\Middleware\Exception;
+
+use OutOfBoundsException;
+
+/**
+ * Class MissingResponseException
+ * @author Mohos Tamás <tomi@mohos.name>
+ * @package Zero\Middleware\Exception
+ */
+class MissingResponseException extends OutOfBoundsException
+{
+
+}
